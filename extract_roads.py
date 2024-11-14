e9889bd0590b78203b643187c8e0bde363311efe
Asgari <marjan.asgari@nrcan-rncan.gc.ca>
2024-11-14 12:44:00 -0500

    grass_project

--- extract_roads.py
+++ extract_roads.py
@@ -10,6 +10,7 @@ from osgeo import gdal, osr
 import platform
 
 gdal.DontUseExceptions()
+MAPSET="PERMANENT"
 
 def cmd_interface(argv=None):
     
@@ -44,8 +45,8 @@ def config_grass():
     # we have to set this path so that Grass_session has acess to libgrass_gis.7.8.dll 
     return grass8bin
 
+
 def road_extraction_grass(input_data, output_data):
-    
     
     # Creating the geofile path for storing the results
     road_dataset_path = Path(input_data)
@@ -63,45 +64,50 @@ def road_extraction_grass(input_data, output_data):
         srs = osr.SpatialReference(wkt=projection)
         srs.AutoIdentifyEPSG()
         epsg = srs.GetAuthorityCode(None)   
-
     if epsg:
-        
+
         start_time = time.time()
         with tempfile.TemporaryDirectory() as gisdb:
-            with Session(gisdb=gisdb, 
-                         location=str(uuid.uuid4()), 
-                         create_opts= f"EPSG:{epsg}"):                
-                # Print current GRASS GIS environment
-                print("--- GRASS GIS - Current GRASS GIS environment ---")
-                print(f"GRASS Version {grass.version().version}")
-                print(grass.gisenv())
-                
-                output_name = "roads"
-                grass.run_command('r.import', input=road_dataset_path, output=output_name, overwrite=True)
-                grass.run_command('g.region', flags='p', raster=output_name)
-                grass.run_command('r.to.vect', input=output_name, output='roads_v', type='line', flags='s')
-                grass.run_command('v.generalize', input='roads_v', output='roads_v_smooth', method='chaiken', threshold=10)
-                grass.run_command('v.clean', input='roads_v_smooth', output='roads_v_smooth_c', tool='break,rmdupl,rmsa')
-                grass.run_command('v.clean', input='roads_v_smooth_c', output='roads_v_smooth_c_s', tool='snap', threshold=3)
-                grass.run_command('v.out.ogr', 
-                            input='roads_v_smooth_c_s', 
-                            output=str(road_geopackage), 
-                            format='GPKG', 
-                            layer='roads_1', 
-                            flags='c', 
-                            overwrite=True)
-                total_time = time.time() - start_time
-                print(
-                    f"------------- Road Extraction for {road_dataset_name} Completed in {(total_time // 60):.0f}m {(total_time % 60):.0f}s -------------"
-                )
-                gc.collect()
+            
+            location = str(uuid.uuid4())
+            mapset = MAPSET
+            
+            if platform.system().lower()  == 'windows':
+                gisrc = os.path.join(gisdb, location, mapset, ".grassrc")
+                os.environ['GISRC'] = gisrc
+            grass.create_project(os.path.join(gisdb, location), epsg=epsg)
+            gsetup.init(gisdb, location, mapset)
+
+            # Print current GRASS GIS environment
+            print("--- GRASS GIS - Current version and environment ---")
+            print(f"GRASS Version {grass.version().version}")
+            print(grass.gisenv())
+            
+            output_name = "roads"
+            grass.run_command('r.import', input=road_dataset_path, output=output_name, overwrite=True)
+            grass.run_command('g.region', flags='p', raster=output_name)
+            grass.run_command('r.to.vect', input=output_name, output='roads_v', type='line', flags='s')
+            grass.run_command('v.generalize', input='roads_v', output='roads_v_smooth', method='chaiken', threshold=10)
+            grass.run_command('v.clean', input='roads_v_smooth', output='roads_v_smooth_c', tool='break,rmdupl,rmsa')
+            grass.run_command('v.clean', input='roads_v_smooth_c', output='roads_v_smooth_c_s', tool='snap', threshold=3)
+            grass.run_command('v.out.ogr', 
+                        input='roads_v_smooth_c_s', 
+                        output=str(road_geopackage), 
+                        format='GPKG', 
+                        layer='roads_1', 
+                        flags='c', 
+                        overwrite=True)
+            total_time = time.time() - start_time
+            print(
+                f"------------- Road Extraction for {road_dataset_name} Completed in {(total_time // 60):.0f}m {(total_time % 60):.0f}s -------------"
+            )
+            gc.collect()
 
     
 if __name__ == "__main__":
 
     
     arguments = cmd_interface()
-    
     if platform.system().lower() == 'windows':
         grass8bin = grass8bin_win = config_grass()
         os.environ['GRASSBIN'] = grass8bin
@@ -109,4 +115,5 @@ if __name__ == "__main__":
     # They should be imported here
     from grass_session import Session
     import grass.script as grass
+    import grass.script.setup as gsetup
     road_extraction_grass(arguments["input"], arguments["output"])
